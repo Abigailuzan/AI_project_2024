124cb0dff694710e839bce5cd16074baf3d111bd
Abigailuzan <80058360+Abigailuzan@users.noreply.github.com>
2024-01-25 01:18:37 +0200

    photo capture

--- index.py
+++ index.py
@@ -1,6 +1,18 @@
-def Try():
-    return "hello world"
+import numpy as np
+import cv2
 
 
-if __name__ == '__main__':
-    print(Try())
+def take_user_photo():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("לא ניתן לפתוח את המצלמה.")
+        return
+    ret, frame = cap.read()
+    cv2.imshow("User Photo", frame)
+    cap.release()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    take_user_photo()
