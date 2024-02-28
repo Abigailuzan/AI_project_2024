c032c9edbf09c13cc3f06b4d52893ace1212f876
Abigailuzan <80058360+Abigailuzan@users.noreply.github.com>
2024-02-29 00:22:38 +0200

    דגכעיחלך

--- yolo_test.py
+++ yolo_test.py
@@ -1,15 +1,28 @@
-from numpy.ma.bench import timer
+import cv2
 from ultralytics import YOLO
 
 # Load a pretrained model
-model = YOLO('bestFoodTaste.pt')
-
+model = YOLO('best.pt')
+image = cv2.imread('11.jpg')
 # Run Inference on the source
-results = model(source='pic/pic3.jpeg', show=True, conf=0.1, save=True) # generator of Results objects
-print(results)
+resultss = model(source='11.jpg', show=True, conf=0.4, save=True) # generator of Results objects
+print(resultss)
 # for result in results:
 #     # If result contains detections, you might need to access attributes or keys directly.
 #     # This is a generic way to print available information in `result` to understand its structure.
 #     print(dir(result))
 #     detected_classes = result.names
 #     print("Detected Classes:", detected_classes)
+
+for results in resultss:
+# ציור מלבן סביב כל אובייקט שזוהה וכתיבת שם האובייקט
+    for result in results.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = result
+
+        if score > 0.2:
+            cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+            cv2.putText(image, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+
+# שמירת התמונה המעובדת
+cv2.imwrite('after_new.jpg', image)
